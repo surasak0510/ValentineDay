6938c4caf27079e9d6347615f969e29f8c943cac
Surasak <92101184+Surasak0510@users.noreply.github.com>
2025-02-10 17:20:00 +0700

    add

---
+++ main.js
@@ -0,0 +1,86 @@
+
+var message = '/message.txt'
+var letterContent = ''
+
+fetch(message)
+    .then(response => response.text())  // อ่านไฟล์เป็นข้อความ
+    .then(data => {
+        // console.log(data);  // แสดงผลใน Console
+        letterContent = data;
+    })
+    .catch(error => {
+        console.error('Error loading file:', error);
+        letterContent = 'error: ' + error.message
+    });
+
+
+durationWrite = 50
+
+
+function effectWrite() {
+    var boxLetter = document.querySelector(".letterContent")
+    letterContentSplited = letterContent.split("")
+
+    letterContentSplited.forEach((val, index) => {
+        setTimeout(() => {
+            boxLetter.innerHTML += val
+        }, durationWrite * index)
+    })
+}
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("active")
+    }, 500)
+})
+
+let audio = document.getElementById("background-music");
+let muteBtn = document.getElementById("muteBtn");
+let volumeSlider = document.getElementById("volumeSlider");
+let volumeTooltip = document.getElementById("volumeTooltip");
+
+// เริ่มต้นให้ Tooltip แสดงค่าปัจจุบัน
+volumeTooltip.textContent = `${Math.round(volumeSlider.value * 100)}%`;
+
+// เล่นเพลงเมื่อคลิก
+function playMusic() {
+    audio.play();
+}
+
+function toggleMute() {
+    let muteIcon = document.getElementById("muteIcon");
+    if (audio.muted) {
+        audio.muted = false;
+        muteIcon.src = "/public/sound_max_fill.svg";
+    } else {
+        audio.muted = true;
+        muteIcon.src = "/public/sound_mute_fill.svg";
+    }
+}
+
+// ปรับระดับเสียงและแสดง Tooltip
+volumeSlider.addEventListener("input", function () {
+    audio.volume = this.value;
+    volumeTooltip.textContent = `${Math.round(this.value * 100)}%`;
+});
+
+
+var openBtn = document.querySelector(".openBtn")
+openBtn.addEventListener("click", () => {
+    document.querySelector(".cardValentine").classList.add("active")
+    document.querySelector(".container").classList.add("close")
+})
+
+var cardValentine = document.querySelector(".cardValentine")
+
+cardValentine.addEventListener("click", () => {
+    cardValentine.classList.toggle("open")
+
+    if (cardValentine.className.indexOf("open") != -1) {
+        setTimeout(effectWrite, 500)
+    } else {
+        setTimeout(() => {
+            document.querySelector(".letterContent").innerHTML = ""
+        }, 1000)
+    }
+})
